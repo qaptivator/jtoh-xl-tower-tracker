77917addfcb88ebe2fc0546c29fe94118c4cf5d0
qaptivator <ac@i-ns.net>
2024-01-11 16:24:58 +0200

    gh pages deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  ssr: false,
+  app: {
+    head: {
+      title: "jtoh-xl-tower-tracker",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content: "JToH XL Project Tower Tracker",
+        },
+      ],
+      link: [{ rel: "icon", type: "image/svg", href: "/favicon.svg" }],
+    },
+    baseURL: "/jtoh-xl-tower-tracker/",
+    buildAssetsDir: "assets",
+  },
+  modules: ["@nuxtjs/tailwindcss"],
+  css: [],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>jtoh-xl-tower-tracker Nuxt3</h1>
+  </div>
+</template>
